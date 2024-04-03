56a5f132daef38a079fc84e72a60697e11f16900
harshilbhatt09 <bhattharshu9503@gmail.com>
2024-04-03 23:23:10 +0530

    update selective test commit

--- data.php
+++ data.php
@@ -33,7 +33,34 @@
         }
     }
     elseif($_REQUEST['btn'] == "update"){
-        $sql = "UPDATE marksheet SET First_Name = '$fname', Last_Name = '$lname', TOC = '$toc', WP = '$wp', DV = '$dv' WHERE Enrollment_Number = '$en_no';";
+        $sql = "UPDATE marksheet SET ";
+        if($fname != null)
+            $sql = $sql."First_Name = '$fname'";
+        if($lname != null){
+        if($fname != null)
+        $sql=$sql.",";
+
+            $sql = $sql."Last_Name = '$lname'";}
+        if($toc != null){
+            if($fname != null || $lname != null) 
+        $sql=$sql.","; 
+            $sql = $sql."TOC = '$toc',";}
+        if($wp != null)
+            $sql = $sql."WP = '$wp',";
+        if($dv != null)
+            $sql = $sql."DV = '$dv',";
+        $sql = $sql." WHERE Enrollment_Number = '$en_no';";
+        
+        echo("$sql");
+        $hi = strpos($sql,",",strlen($sql)-10);
+        // echo(strlen($sql)-1);
+        if($hi == false){
+            echo("$hi");
+        }
+        else{
+            echo("hello".$hi);
+        }
+        // $sql = "UPDATE marksheet SET First_Name = '$fname', Last_Name = '$lname', TOC = '$toc', WP = '$wp', DV = '$dv' WHERE Enrollment_Number = '$en_no';";
         if(mysqli_query($conn, "$sql")){
             echo("Succesfully updated");
         }
